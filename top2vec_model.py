665793f249884a56359666a475879fc84a411549
George Araujo <george.gcac@gmail.com>
2022-12-27 19:38:37 -0300

    Added support for training top2vec model on specific conference and/or year
    Signed-off-by: George Araujo <george.gcac@gmail.com>

--- top2vec_model.py
+++ top2vec_model.py
@@ -13,13 +13,36 @@ from utils import conferences_pdfs, recreate_url, setup_log
 _logger = logging.getLogger(__name__)
 
 
-def _create_corpus(separator: str) -> None:
-    corpus_files = [Path(f'data/{c}/pdfs_clean.csv') for c in conferences_pdfs]
-    url_files = [Path(f'data/{c}/paper_info.csv') for c in conferences_pdfs]
+def _create_corpus(separator: str, conference: str, year: int) -> None:
+    if len(conference) > 0 and year > 0:
+        corpus_files = [Path(f'data/{c}/pdfs_clean.csv') for c in conferences_pdfs if c == f'{conference}/{year}']
+        url_files = [Path(f'data/{c}/paper_info.csv') for c in conferences_pdfs if c == f'{conference}/{year}']
 
-    all_titles = Path(f'data/papers_titles.txt').open('w')
-    all_texts = Path(f'data/papers_contents.txt').open('w')
-    all_urls = Path(f'data/papers_urls.txt').open('w')
+        all_titles = Path(f'data/{conference}_{year}_papers_titles.txt').open('w')
+        all_texts = Path(f'data/{conference}_{year}_papers_contents.txt').open('w')
+        all_urls = Path(f'data/{conference}_{year}_papers_urls.txt').open('w')
+    elif len(conference) > 0:
+        corpus_files = [Path(f'data/{c}/pdfs_clean.csv') for c in conferences_pdfs if c.startswith(conference)]
+        url_files = [Path(f'data/{c}/paper_info.csv') for c in conferences_pdfs if c.startswith(conference)]
+
+        all_titles = Path(f'data/{conference}_papers_titles.txt').open('w')
+        all_texts = Path(f'data/{conference}_papers_contents.txt').open('w')
+        all_urls = Path(f'data/{conference}_papers_urls.txt').open('w')
+    elif year > 0:
+        corpus_files = [Path(f'data/{c}/pdfs_clean.csv') for c in conferences_pdfs if c.endswith(str(year))]
+        url_files = [Path(f'data/{c}/paper_info.csv') for c in conferences_pdfs if c.endswith(str(year))]
+
+        all_titles = Path(f'data/{year}_papers_titles.txt').open('w')
+        all_texts = Path(f'data/{year}_papers_contents.txt').open('w')
+        all_urls = Path(f'data/{year}_papers_urls.txt').open('w')
+    else:
+        corpus_files = [Path(f'data/{c}/pdfs_clean.csv') for c in conferences_pdfs]
+        url_files = [Path(f'data/{c}/paper_info.csv') for c in conferences_pdfs]
+
+        all_titles = Path(f'data/papers_titles.txt').open('w')
+        all_texts = Path(f'data/papers_contents.txt').open('w')
+        all_urls = Path(f'data/papers_urls.txt').open('w')
+
 
     pbar_files = tqdm(corpus_files)
     titles_set = set()
@@ -58,9 +81,23 @@ def _create_corpus(separator: str) -> None:
     all_texts.close()
     all_urls.close()
 
-def _train_top2vec_model(speed: str) -> None:
-    all_titles = Path(f'data/papers_titles.txt').read_text().strip().split('\n')
-    all_texts = Path(f'data/papers_contents.txt').read_text().strip().split('\n')
+
+def _train_top2vec_model(speed: str, conference: str, year: int) -> None:
+    if len(conference) > 0 and year > 0:
+        all_titles = Path(f'data/{conference}_{year}_papers_titles.txt')
+        all_texts = Path(f'data/{conference}_{year}_papers_contents.txt')
+    elif len(conference) > 0:
+        all_titles = Path(f'data/{conference}_papers_titles.txt')
+        all_texts = Path(f'data/{conference}_papers_contents.txt')
+    elif year > 0:
+        all_titles = Path(f'data/{year}_papers_titles.txt')
+        all_texts = Path(f'data/{year}_papers_contents.txt')
+    else:
+        all_titles = Path(f'data/papers_titles.txt')
+        all_texts = Path(f'data/papers_contents.txt')
+
+    all_titles = all_titles.read_text().strip().split('\n')
+    all_texts = all_texts.read_text().strip().split('\n')
 
     _logger.print(f'Found {len(all_titles):n} titles')
     _logger.print(f'Found {len(all_texts):n} papers')
@@ -76,11 +113,21 @@ def _train_top2vec_model(speed: str) -> None:
             workers=cpu_count()//2,
         )
 
-    model.save(f'model_data/top2vec_model_{speed}')
+    if len(conference) > 0 and year > 0:
+        model.save(f'model_data/top2vec_model_{speed}_{conference}_{year}')
+    elif len(conference) > 0:
+        model.save(f'model_data/top2vec_model_{speed}_{conference}')
+    elif year > 0:
+        model.save(f'model_data/top2vec_model_{speed}_{year}')
+    else:
+        model.save(f'model_data/top2vec_model_{speed}')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Train top2vec model.")
+    parser.add_argument('--conference', type=str, default='',
+                        help='use papers from this conference only when creating corpus')
     parser.add_argument('-c', '--create_corpus', action='store_true',
                         help='create single corpus with entire text from all papers')
     parser.add_argument('-l', '--log_level', type=str, default='warning',
@@ -97,23 +144,42 @@ if __name__ == '__main__':
                         help='search for topics related to each of the given words')
     parser.add_argument('-t', '--train', action='store_true',
                         help='train top2vec model')
+    parser.add_argument('--year', type=int, default=0,
+                        help='use papers from this year only when creating corpus')
     args = parser.parse_args()
 
     log_dir = Path('logs/').expanduser()
     log_dir.mkdir(exist_ok=True)
-    setup_log(args.log_level, log_dir / 'top2vec.log')
+
+    if len(args.conference) > 0 and args.year > 0:
+        setup_log(args.log_level, log_dir / f'top2vec_{args.conference}_{args.year}.log')
+    elif len(args.conference) > 0:
+        setup_log(args.log_level, log_dir / f'top2vec_{args.conference}.log')
+    elif args.year > 0:
+        setup_log(args.log_level, log_dir / f'top2vec_{args.year}.log')
+    else:
+        setup_log(args.log_level, log_dir / 'top2vec.log')
 
     if args.create_corpus:
-        _create_corpus(args.separator)
+        _create_corpus(args.separator, args.conference, args.year)
 
     if args.train:
-        _train_top2vec_model(args.speed)
+        _train_top2vec_model(args.speed, args.conference, args.year)
 
-    model = Top2Vec.load(f'model_data/top2vec_model_{args.speed}')
+    if len(args.conference) > 0 and args.year > 0:
+        model = Top2Vec.load(f'model_data/top2vec_model_{args.speed}_{args.conference}_{args.year}')
+        _logger.print(f'Found {model.get_num_topics()} topics for {args.conference} {args.year}')
+    elif len(args.conference) > 0:
+        model = Top2Vec.load(f'model_data/top2vec_model_{args.speed}_{args.conference}')
+        _logger.print(f'Found {model.get_num_topics()} topics for {args.conference}')
+    elif args.year > 0:
+        model = Top2Vec.load(f'model_data/top2vec_model_{args.speed}_{args.year}')
+        _logger.print(f'Found {model.get_num_topics()} topics for {args.year}')
+    else:
+        model = Top2Vec.load(f'model_data/top2vec_model_{args.speed}')
+        _logger.print(f'Found {model.get_num_topics()} topics')
 
     # printing information about model
-    _logger.print(f'Found {model.get_num_topics()} topics')
-
     topic_sizes, _ = model.get_topic_sizes()
     topic_words, word_scores, topic_nums = model.get_topics()
 
