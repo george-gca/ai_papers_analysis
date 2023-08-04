52c0da2c7b4f06333861559e965cf8c85c860c83
George Araujo <george.gcac@gmail.com>
2023-08-04 14:34:59 -0300

    Added support for using only abstracts and dirty version
    Signed-off-by: George Araujo <george.gcac@gmail.com>

--- top2vec_model.py
+++ top2vec_model.py
@@ -13,88 +13,108 @@ from utils import conferences_pdfs, recreate_url, setup_log
 _logger = logging.getLogger(__name__)
 
 
-def _create_corpus(separator: str, conference: str, year: int) -> None:
+def _create_corpus(separator: str, conference: str, year: int, abstracts_only: bool=False, clean_file: bool=True) -> None:
+    if abstracts_only:
+        text_file = 'abstracts'
+        output_content = 'abstracts'
+
+    else:
+        text_file = 'pdfs'
+        output_content = 'contents'
+
+    if clean_file:
+        text_file += '_clean.csv'
+        output_content += '_clean'
+    else:
+        text_file += '.csv'
+
     if len(conference) > 0 and year > 0:
-        corpus_files = [Path(f'data/{c}/pdfs_clean.csv') for c in conferences_pdfs if c == f'{conference}/{year}']
+        corpus_files = [Path(f'data/{c}/{text_file}') for c in conferences_pdfs if c == f'{conference}/{year}']
         url_files = [Path(f'data/{c}/paper_info.csv') for c in conferences_pdfs if c == f'{conference}/{year}']
 
         all_titles = Path(f'data/{conference}_{year}_papers_titles.txt').open('w')
-        all_texts = Path(f'data/{conference}_{year}_papers_contents.txt').open('w')
+        all_texts = Path(f'data/{conference}_{year}_papers_{output_content}.txt').open('w')
         all_urls = Path(f'data/{conference}_{year}_papers_urls.txt').open('w')
     elif len(conference) > 0:
-        corpus_files = [Path(f'data/{c}/pdfs_clean.csv') for c in conferences_pdfs if c.startswith(conference)]
+        corpus_files = [Path(f'data/{c}/{text_file}') for c in conferences_pdfs if c.startswith(conference)]
         url_files = [Path(f'data/{c}/paper_info.csv') for c in conferences_pdfs if c.startswith(conference)]
 
         all_titles = Path(f'data/{conference}_papers_titles.txt').open('w')
-        all_texts = Path(f'data/{conference}_papers_contents.txt').open('w')
+        all_texts = Path(f'data/{conference}_papers_{output_content}.txt').open('w')
         all_urls = Path(f'data/{conference}_papers_urls.txt').open('w')
     elif year > 0:
-        corpus_files = [Path(f'data/{c}/pdfs_clean.csv') for c in conferences_pdfs if c.endswith(str(year))]
+        corpus_files = [Path(f'data/{c}/{text_file}') for c in conferences_pdfs if c.endswith(str(year))]
         url_files = [Path(f'data/{c}/paper_info.csv') for c in conferences_pdfs if c.endswith(str(year))]
 
         all_titles = Path(f'data/{year}_papers_titles.txt').open('w')
-        all_texts = Path(f'data/{year}_papers_contents.txt').open('w')
+        all_texts = Path(f'data/{year}_papers_{output_content}.txt').open('w')
         all_urls = Path(f'data/{year}_papers_urls.txt').open('w')
     else:
-        corpus_files = [Path(f'data/{c}/pdfs_clean.csv') for c in conferences_pdfs]
+        corpus_files = [Path(f'data/{c}/{text_file}') for c in conferences_pdfs]
         url_files = [Path(f'data/{c}/paper_info.csv') for c in conferences_pdfs]
 
         all_titles = Path('data/papers_titles.txt').open('w')
-        all_texts = Path('data/papers_contents.txt').open('w')
+        all_texts = Path('data/papers_{output_content}.txt').open('w')
         all_urls = Path('data/papers_urls.txt').open('w')
 
 
-    pbar_files = tqdm(corpus_files)
     titles_set = set()
 
-    for i, (corpus_file, url_file) in enumerate(zip(pbar_files, url_files)):
-        pbar_files.set_description(str(corpus_file.parents[0]).replace(str(corpus_file.parents[2]), '')[1:])
-        if len(separator) == 1:
-            df = pd.read_csv(corpus_file, sep=separator, dtype=str, keep_default_na=False)
-        else:
-            df = pd.read_csv(corpus_file, sep=separator, dtype=str, engine='python', keep_default_na=False)
+    with tqdm(corpus_files) as pbar_files:
+        for i, (corpus_file, url_file) in enumerate(zip(pbar_files, url_files)):
+            pbar_files.set_description(str(corpus_file.parents[0]).replace(str(corpus_file.parents[2]), '')[1:])
+            if len(separator) == 1:
+                df = pd.read_csv(corpus_file, sep=separator, dtype=str, keep_default_na=False)
+            else:
+                df = pd.read_csv(corpus_file, sep=separator, dtype=str, engine='python', keep_default_na=False)
 
-        df_url = pd.read_csv(url_file, sep=';', dtype=str, keep_default_na=False)
-        if len(df) < len(df_url):
-                # drop extra urls
-            papers_titles = set(df['title'])
-            df_url = df_url[df_url['title'].isin(papers_titles)]
+            df_url = pd.read_csv(url_file, sep=';', dtype=str, keep_default_na=False)
+            if len(df) < len(df_url):
+                    # drop extra urls
+                papers_titles = set(df['title'])
+                df_url = df_url[df_url['title'].isin(papers_titles)]
 
-        assert len(df) == len(df_url), f'df ({len(df)}) and df_url ({len(df_url)}) should have same size'
-        df = df.join(df_url['abstract_url'].astype(str))
+            assert len(df) == len(df_url), f'df ({len(df)}) and df_url ({len(df_url)}) should have same size'
+            df = df.join(df_url['abstract_url'].astype(str))
 
-        for title, text, url in zip(tqdm(df['title'], leave=False), df['paper'], df['abstract_url']):
-            if title.lower() in titles_set:
-                continue
+            for title, text, url in zip(tqdm(df['title'], leave=False), df['paper'], df['abstract_url']):
+                if title.lower() in titles_set:
+                    continue
 
-            titles_set.add(title.lower())
-            all_titles.write(f'{title}\n')
-            all_texts.write(f'{text}\n')
-            conf, year = conferences_pdfs[i].split('/')
-            all_urls.write(f'{recreate_url(str(url), conf, int(year), is_abstract=True)}\n')
+                titles_set.add(title.lower())
+                all_titles.write(f'{title}\n')
+                all_texts.write(f'{text}\n')
+                conf, year = conferences_pdfs[i].split('/')
+                all_urls.write(f'{recreate_url(str(url), conf, int(year), is_abstract=True)}\n')
 
-        all_titles.flush()
-        all_texts.flush()
-        all_urls.flush()
+            all_titles.flush()
+            all_texts.flush()
+            all_urls.flush()
 
     all_titles.close()
     all_texts.close()
     all_urls.close()
 
 
-def _train_top2vec_model(speed: str, conference: str, year: int) -> None:
-    if len(conference) > 0 and year > 0:
-        all_titles = Path(f'data/{conference}_{year}_papers_titles.txt')
-        all_texts = Path(f'data/{conference}_{year}_papers_contents.txt')
-    elif len(conference) > 0:
-        all_titles = Path(f'data/{conference}_papers_titles.txt')
-        all_texts = Path(f'data/{conference}_papers_contents.txt')
-    elif year > 0:
-        all_titles = Path(f'data/{year}_papers_titles.txt')
-        all_texts = Path(f'data/{year}_papers_contents.txt')
+def _train_top2vec_model(speed: str, conference: str, year: int, abstracts_only: bool=False, clean_file: bool=True) -> None:
+    if abstracts_only:
+        output_content = 'abstracts'
+
     else:
-        all_titles = Path('data/papers_titles.txt')
-        all_texts = Path('data/papers_contents.txt')
+        output_content = 'contents'
+
+    if clean_file:
+        output_content += '_clean'
+
+    prefix = ''
+
+    if len(conference) > 0:
+        prefix += f'{conference}_'
+    if year > 0:
+        prefix += f'{year}_'
+
+    all_titles = Path(f'data/{prefix}papers_titles.txt')
+    all_texts = Path(f'data/{prefix}papers_{output_content}.txt')
 
     all_titles = all_titles.read_text().strip().split('\n')
     all_texts = all_texts.read_text().strip().split('\n')
@@ -113,19 +133,23 @@ def _train_top2vec_model(speed: str, conference: str, year: int) -> None:
             workers=cpu_count()//2,
         )
 
-    if len(conference) > 0 and year > 0:
-        model.save(f'model_data/top2vec_model_{speed}_{conference}_{year}')
-    elif len(conference) > 0:
-        model.save(f'model_data/top2vec_model_{speed}_{conference}')
-    elif year > 0:
-        model.save(f'model_data/top2vec_model_{speed}_{year}')
-    else:
-        model.save(f'model_data/top2vec_model_{speed}')
+    suffix = ''
+
+    if len(conference) > 0:
+        suffix += f'_{conference}'
+    if year > 0:
+        suffix += f'_{year}'
+
+    model.save(f'model_data/top2vec_model_{speed}{suffix}')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Train top2vec model.")
+    parser.add_argument('--abstracts_only', action='store_true',
+                        help='use only abstracts when creating corpus')
+    parser.add_argument('--clean_file', action='store_false',
+                        help='use clean file when creating corpus')
     parser.add_argument('--conference', type=str, default='',
                         help='use papers from this conference only when creating corpus')
     parser.add_argument('-c', '--create_corpus', action='store_true',
@@ -151,22 +175,24 @@ if __name__ == '__main__':
     log_dir = Path('logs/').expanduser()
     log_dir.mkdir(exist_ok=True)
 
-    if len(args.conference) > 0 and args.year > 0:
-        suffix = f'_{args.conference}_{args.year}'
-    elif len(args.conference) > 0:
-        suffix = f'_{args.conference}'
-    elif args.year > 0:
-        suffix = f'_{args.year}'
-    else:
-        suffix = ''
+    suffix = ''
+
+    if len(args.conference) > 0:
+        suffix += f'_{args.conference}'
+    if args.year > 0:
+        suffix += f'_{args.year}'
+    if args.abstracts_only:
+        suffix += '_abstracts'
+    if not args.clean_file:
+        suffix += '_raw'
 
     setup_log(args.log_level, log_dir / f'top2vec{suffix}.log')
 
     if args.create_corpus:
-        _create_corpus(args.separator, args.conference, args.year)
+        _create_corpus(args.separator, args.conference, args.year, args.abstracts_only, args.clean_file)
 
     if args.train:
-        _train_top2vec_model(args.speed, args.conference, args.year)
+        _train_top2vec_model(args.speed, args.conference, args.year, args.abstracts_only, args.clean_file)
 
     if len(args.conference) > 0 and args.year > 0:
         model = Top2Vec.load(f'model_data/top2vec_model_{args.speed}_{args.conference}_{args.year}')
